d9096f9a75a769a74cfff25c4f9aae951758f74d
주열 <dlwnduf84@gmail.com>
2026-04-16 20:27:13 +0900

    update

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001 > nul
+echo ===================================
+echo      ControlCentor 실행기
+echo ===================================
+echo.
+echo [1/2] 최신 버전 확인 중...
+git pull
+echo.
+echo [2/2] ControlCentor 실행 중...
+python AutogameCentor/Gui.py
+echo.
+pause
